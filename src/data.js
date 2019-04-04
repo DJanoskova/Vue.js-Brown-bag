e8aeaa05b1bdf9b34d7436993369482e2f85a807
Dana Janoskova <dana.janoskova@accenture.com>
2019-04-04 20:03:50 +0200

    autopush

--- src/data.js
+++ src/data.js
@@ -575,3 +575,137 @@ const myComponent = () => {
   return <div>{view}<br />{route}</div>;
 }
 `
+
+export const validatingInputsExample = `<ValidatingInput v-model="user.username"
+  validate="required|min:4"
+  name="username" />
+  
+<ValidatingInput v-model="user.age"
+  validate="required|integer"
+  name="age" />
+  
+<ValidatingInput v-model="user.email"
+  validate="required|email"
+  name="email" />
+  
+<ValidatingInput v-model="user.about"
+  validate="max:50"
+  name="about" />
+  
+<ValidatingButton>Submit</ValidatingButton>`
+
+export const validatingInputSource = `<template>
+  <div class="mb-3">
+    <input v-model="model"
+      :type="type"
+      :name="name"
+      v-validate.initial="validate"
+      data-vv-as=" "
+      :data-vv-name="name"
+      :placeholder="placeholder"
+      class="form-control"
+      :class="customClasses">
+
+    <div v-if="displayedError" class="small text-danger">
+      {{ displayedError }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String || Number
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    name: {
+      type: String
+    },
+    validate: {
+      type: String || Object
+    },
+    placeholder: {
+      type: String
+    }
+  },
+  data () {
+    return {
+      model: this.value
+    }
+  },
+  watch: {
+    value () {
+      this.model = this.value
+    },
+    model () {
+      this.$emit('input', this.model)
+    }
+  },
+  computed: {
+    displayedError () {
+      const errors = this.errors;
+      if (!errors || !errors.items || !errors.items.length) return null;
+      const field = this.fields[this.name];
+      if (!field || !field.touched) return null;
+      const hasError = errors.items.find(item => item.field === this.name);
+      if (hasError) return hasError.msg;
+      return null;
+    },
+    customClasses () {
+      const field = this.fields[this.name];
+      if (!field || !field.touched) return null;
+
+      const classes = [];
+      if (field.valid) classes.push('border-success');
+      else classes.push('border-danger');
+      return classes;
+    },
+    showError () {
+      const field = this.fields[this.name];
+      if (!field) return false;
+      return field.touched && !field.valid;
+    }
+  }
+}
+</script>`
+
+export const validatingButtonSource = `<template>
+  <button :type="type"
+    class="btn"
+    :class="'btn-' + color"
+    :disabled="disabled || errors.items.length > 0">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'submit'
+    },
+    color: {
+      type: String,
+      default: 'primary'
+    },
+    submitText: {
+      type: String,
+      default: 'Submit'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      children: []
+    }
+  }
+}
+</script>`
--- src/views/pages/CustomValidatingInput.vue
+++ src/views/pages/CustomValidatingInput.vue
@@ -7,24 +7,43 @@
         <ValidatingInput v-model="user.age" validate="required|integer" placeholder="Age" name="age" />
         <ValidatingInput v-model="user.email" validate="required|email" placeholder="E-mail" name="email" />
         <ValidatingInput v-model="user.about" validate="max:50" placeholder="About" name="about" />
-
         <ValidatingButton class="">Submit</ValidatingButton>
 
         <hr />
 
         <pre>{{ user }}</pre>
+
+        <Codemirror v-model="validatingInputsExample" lang="html" />
+      </div>
+
+      <div class="col-12 col-lg-6">
+        <div class="text-muted mb-1">
+          main.js
+        </div>
+        <pre>import VeeValidate from 'vee-validate'
+
+Vue.use(VeeValidate)</pre>
+
+        <div class="text-muted mb-1">
+          ValidatingInput.vue
+        </div>
+        <Codemirror v-model="validatingInputSource" />
+
+        <div class="text-muted mb-1 mt-2">
+          ValidatingButton.vue
+        </div>
+        <Codemirror v-model="validatingButtonSource" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import Codemirror from '../../components/Codemirror'
-// import ButtonPicker from '../../components/ButtonPicker'
+import Codemirror from '../../components/Codemirror'
 import ValidatingInput from '../../components/ValidatingInput'
 import ValidatingButton from '../../components/ValidatingButton'
 
-// import { buttonPickerSource, buttonPickerParentSource } from '../../data';
+import { validatingInputsExample, validatingInputSource, validatingButtonSource } from '../../data';
 
 export default {
   data() {
@@ -34,12 +53,16 @@ export default {
         age: '',
         email: '',
         about: ''
-      }
+      },
+      validatingInputsExample,
+      validatingInputSource,
+      validatingButtonSource
     }
   },
   components: {
     ValidatingInput,
-    ValidatingButton
+    ValidatingButton,
+    Codemirror
   }
 }
 </script>
